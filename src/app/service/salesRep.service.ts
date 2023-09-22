d8dd2c40c3bee72873656bc5b42ab25e0b5535cd
melvine <atienomelvine98@gmail.com>
2023-09-22 09:21:07 +0300

    Changes on the routes table Added the salesRep service

--- src/app/service/salesRep.service.ts
+++ src/app/service/salesRep.service.ts
@@ -1,10 +1,33 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { CustomResponse } from "../interface/custom-response";
+import { Observable, catchError, tap, throwError } from "rxjs";
+import { SalesRep } from "../interface/salesRep";
 
 @Injectable({
     providedIn: 'root'
 })
-export class CustomerService {
+export class salesRepService {
+    private readonly apiUrl = 'http://localhost:8080';
     
     constructor(private http: HttpClient) {}
+
+    salesReps$ = <Observable<CustomResponse>>
+        this.http.get<CustomResponse>(`${this.apiUrl}/api/salesRep/list`)
+            .pipe(
+                tap(response => {
+                    console.log("Fetched customer data:", response);
+                }),
+                catchError(this.handleError)
+            );
+
+    savecustomer(salesrep: SalesRep): Observable<any> {
+        return this.http.post <any> (`${this.apiUrl}/api/salesRep/save`, salesrep, {observe:'response'});
+    }
+
+    
+    private handleError(error: HttpErrorResponse): Observable<never> {
+        console.log(error);
+        return throwError(`An error occurred - Error code: ${error.status}`);
+    }
 }
